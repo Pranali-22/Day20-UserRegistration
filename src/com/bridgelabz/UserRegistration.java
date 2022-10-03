496d82c9b6a8ed5aa44472bbcb66a03f71bd527b
Pranali-22 <lpranali22@gmail.com>
2022-10-03 10:58:21 +0530

    Refactored : Changed regex and method in test class

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -3,40 +3,57 @@
  */
 package com.bridgelabz;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author Dell
  * As a User need to follow pre-defined Password rules.
- * Rule1 – minimum 8 Characters - NOTE – All rules must
+ * Rule1 – minimum 8 Characters 
  */
 public class UserRegistration {
 	
 	//Validate first name
 	public boolean validateFirstName(String firstName) {
-		String nameRegex =  "^[A-Z]{1}[a-z]{2,}$";
-		 return firstName.matches(nameRegex);
+		String newRegex =  "[A-Z]{1}[a-z]{2,}$";
+		Pattern patternObj = Pattern.compile(newRegex);
+		 
+		Matcher matcherObj = patternObj.matcher(firstName);
+		return matcherObj.matches();
 	}
 	
 	//Validate last name
 	public boolean validateLastName(String lastName) {
-		String nameRegex =  "^[A-Z]{1}[a-z]{2,}$";
-		 return lastName.matches(nameRegex);
+		String newRegex =  "[A-Z]{1}[a-z]{2,}$";
+		Pattern patternObj = Pattern.compile(newRegex);
+		 
+		Matcher matcherObj = patternObj.matcher(lastName);
+		return matcherObj.matches();
 	}
 	
 	//Validate email address
 	public boolean validateEmailAddress(String emailId) {
-		String nameRegex = "^[a-z]{1,}(.[a-z]{1,})?@bl.co(.in)?$";
-		return emailId.matches(nameRegex);
+		String newRegex = "[a-z]{1,}(.[a-z]{1,})?@bl.co(.in)?$";
+		Pattern patternObj = Pattern.compile(newRegex);
+		 
+		Matcher matcherObj = patternObj.matcher(emailId);
+		return matcherObj.matches();	 
 	}
-	
+
 	//Validate mobile number
 	public boolean validateMobileNumber(String mobileNumber) {
-		String nameRegex = "[0-9]{2} [0-9]{10}$";
-		return mobileNumber.matches(nameRegex);
+		String newRegex = "[0-9]{2} [0-9]{10}$";
+		Pattern patternObj = Pattern.compile(newRegex);
+		 
+		Matcher matcherObj = patternObj.matcher(mobileNumber);
+		return matcherObj.matches();	 
 	}
 
 	//Validate password
 	public boolean validatePassword(String password) {
-		String nameRegex = "[a-zA-Z0-9!@#$&*]{8,}$";
-		return password.matches(nameRegex);
+		String newRegex = "[a-zA-Z0-9!@#$&*]{8,}$";
+		Pattern patternObj = Pattern.compile(newRegex);		 
+		Matcher matcherObj = patternObj.matcher(password);
+		return matcherObj.matches();
 	}
 }
--- src/com/bridgelabz/UserRegistrationTest.java
+++ src/com/bridgelabz/UserRegistrationTest.java
@@ -5,12 +5,13 @@ package com.bridgelabz;
 import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 /**
  * @author Dell
- * As a User need to follow pre-defined Mobile Format - E.g. 91 9919819801 - Country code follow by space
- *
+ * As a User need to follow pre-defined Password rules.
+ * Rule1 – minimum 8 Characters 
  */
 class UserRegistrationTest {
 
@@ -25,13 +26,8 @@ class UserRegistrationTest {
 	void validate_given_first_name() {		
 		
 		System.out.println("Enter first name");
-		String firstName=input.nextLine();
-		
-		if(userRegistration.validateFirstName(firstName)) {
-			System.out.println("First name is valid");
-		}else {
-			System.out.println("First name is not valid");
-		}
+		String firstName=input.nextLine();		
+		Assert.assertTrue(userRegistration.validateFirstName(firstName));
 	}
 	
 	/**
@@ -42,13 +38,8 @@ class UserRegistrationTest {
 	void validate_given_last_name() {
 		
 		System.out.println("Enter last name");
-		String lastName=input.nextLine();
-		
-		if(userRegistration.validateLastName(lastName)) {
-			System.out.println("Last name is valid");
-		}else {
-			System.out.println("Last name is not valid");
-		}
+		String lastName=input.nextLine();		
+		Assert.assertTrue(userRegistration.validateLastName(lastName));
 	}
 	
 	
@@ -60,13 +51,8 @@ class UserRegistrationTest {
 	void validate_given_email_address() {
 		
 		System.out.println("Enter email address");
-		String emailId=input.nextLine();
-		
-		if(userRegistration.validateEmailAddress(emailId)) {
-			System.out.println("Email address is valid");
-		}else {
-			System.out.println("Email address is not valid");
-		}
+		String emailId=input.nextLine();		
+		Assert.assertTrue(userRegistration.validateEmailAddress(emailId));
 	}
 	
 	
@@ -78,16 +64,10 @@ class UserRegistrationTest {
 	void validate_given_mobile_number() {
 		
 		System.out.println("Enter Mobile number");
-		String mobileNumber=input.nextLine();
-		
-		if(userRegistration.validateMobileNumber(mobileNumber)) {
-			System.out.println("Mobile number is valid");
-		}else {
-			System.out.println("Mobile number is not valid");
-		}
+		String mobileNumber=input.nextLine();		
+		Assert.assertTrue(userRegistration.validateMobileNumber(mobileNumber));
 	}
 	
-
 	/**
 	 * Test method for validatePassword
 	 * validate Password
@@ -96,12 +76,7 @@ class UserRegistrationTest {
 	void validate_given_password() {
 		
 		System.out.println("Enter password");
-		String password=input.nextLine();
-		
-		if(userRegistration.validatePassword(password)) {
-			System.out.println("Password is valid");
-		}else {
-			System.out.println("Password is not valid");
-		}
+		String password=input.nextLine();		
+		Assert.assertTrue(userRegistration.validatePassword(password));
 	}
 }
