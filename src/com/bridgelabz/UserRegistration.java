54094b9fe4fef458ea9e0a0234ca3c987f665dfc
Pranali-22 <lpranali22@gmail.com>
2022-10-06 22:35:44 +0530

    Added: Validate User Entry

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -11,6 +11,15 @@ import java.util.regex.Pattern;
  * UC9 - Clear all email samples provided separately
  */
 public class UserRegistration {
+		
+	//Validate user entry
+	public String VaidateUserEntry(Boolean isValidEntry) {
+		if(isValidEntry) {
+			return "Happy";
+		}
+		return "Sad";
+	}
+	
 	
 	//Validate first name
 	public boolean validateFirstName(String firstName) {
@@ -19,7 +28,7 @@ public class UserRegistration {
 		Matcher matcherObj = patternObj.matcher(firstName);
 		return matcherObj.matches();
 	}
-				
+					
 	//Validate last name
 	public boolean validateLastName(String lastName) {
 		String newRegex =  "[A-Z]{1}[a-z]{2,}$";
@@ -27,15 +36,15 @@ public class UserRegistration {
 		Matcher matcherObj = patternObj.matcher(lastName);
 		return matcherObj.matches();
 	}
-				
+					
 	//Validate email address
 	public boolean validateEmailAddress(String emailId) {
-		String newRegex = "[a-z-+-.]@[a-z0-9].[a-z](.[a-z]{2,})?$";
+		String newRegex = "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
 		Pattern patternObj = Pattern.compile(newRegex);			 
 		Matcher matcherObj = patternObj.matcher(emailId);
 		return matcherObj.matches();	 
 	}
-
+	
 	//Validate mobile number
 	public boolean validateMobileNumber(String mobileNumber) {
 		String newRegex = "[0-9]{2} [0-9]{10}$";
@@ -43,7 +52,7 @@ public class UserRegistration {
 		Matcher matcherObj = patternObj.matcher(mobileNumber);
 		return matcherObj.matches();	 
 	}
-		
+			
 	//Validate password to have exact 1 special character
 	public boolean validatePassword(String password) {
 		String newRegex = "[A-Z]{1,}[a-zA-Z0-9]{5,}[!@#$&*]{1}[0-9]{1,}$";
@@ -51,4 +60,5 @@ public class UserRegistration {
 		Matcher matcherObj = patternObj.matcher(password);
 		return matcherObj.matches();
 	}
+	
 }
--- src/com/bridgelabz/UserRegistrationTest.java
+++ src/com/bridgelabz/UserRegistrationTest.java
@@ -15,30 +15,36 @@ import org.junit.jupiter.api.Test;
 class UserRegistrationTest {
 
 	Scanner input = new Scanner(System.in);
+		
 	UserRegistration userRegistration = new UserRegistration();
+	Boolean isValidFirstName = true, isValidLastName = true, isValidEmailId = true, isValidMobileNumber = true, isValidPassword = true;
 	
-	/**
+	/*
 	 * Test method for {@link com.bridgelabz.UserRegistration#validateFirstName(java.lang.String)}.
 	 * validate first name 
 	 */
+	
 	@Test
 	void validate_given_first_name() {		
 		
 		System.out.println("Enter first name");
 		String firstName=input.nextLine();		
 		Assert.assertTrue(userRegistration.validateFirstName(firstName));
+		isValidFirstName = userRegistration.validateFirstName(firstName);
 	}
 	
 	/**
 	 * Test method for validateLastName
 	 * validate last name 
 	 */
+	
 	@Test
 	void validate_given_last_name() {
 		
 		System.out.println("Enter last name");
 		String lastName=input.nextLine();		
 		Assert.assertTrue(userRegistration.validateLastName(lastName));
+		isValidLastName = userRegistration.validateLastName(lastName);
 	}
 	
 	
@@ -46,13 +52,13 @@ class UserRegistrationTest {
 	 * Test method for validateEmailAddress
 	 * validate email address 
 	 */	
+	
 	@Test
 	void validate_given_email_address() {
-			
-		String[] emailList = {"abc@yahoo.com","abc-100@abc.net","abc.100@abc.com.au","abc@1.com","abc@.com.my","abc","abc()*@gmail.com","abc123@gmail.a"};
-		for(int i=0; i< emailList.length; i++) {	
-			Assert.assertTrue(userRegistration.validateEmailAddress(emailList[i]));
-		}
+		System.out.println("Enter email Id");
+		String emailId=input.nextLine();			
+		Assert.assertTrue(userRegistration.validateEmailAddress(emailId));
+		isValidEmailId = userRegistration.validateEmailAddress(emailId);
 	}
 	
 	
@@ -60,23 +66,36 @@ class UserRegistrationTest {
 	 * Test method for validateMobileNumber
 	 * validate Mobile Number
 	 */
+	
 	@Test
 	void validate_given_mobile_number() {
 		
 		System.out.println("Enter Mobile number");
 		String mobileNumber=input.nextLine();		
 		Assert.assertTrue(userRegistration.validateMobileNumber(mobileNumber));
+		isValidMobileNumber = userRegistration.validateMobileNumber(mobileNumber);
 	}
 	
 	/**
 	 * Test method for validatePassword
 	 * validate Password - to have exact 1 special character
 	 */
-	@Test
+	@Test	
 	void validate_given_password() {
 		
 		System.out.println("Enter password");
 		String password=input.nextLine();		
 		Assert.assertTrue(userRegistration.validatePassword(password));
+		isValidPassword = userRegistration.validatePassword(password);
+	}
+	
+	/**
+	 * Test method for validatePassword
+	 * Validate a valid user entry
+	 */
+	@Test	
+	void validate_user_entry() {
+		Assert.assertEquals("Happy",userRegistration.VaidateUserEntry(isValidFirstName && isValidLastName && isValidEmailId && isValidMobileNumber && isValidPassword));
+			
 	}
 }
